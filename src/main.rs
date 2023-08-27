d79a22140df05cc0e9a3937b116d16048da8a48a
nordluma <maxnordlund.95@hotmail.com>
2023-08-27 16:11:44 +0300

    feat: parse cli arguments

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@ use cli_db::args::CliArgs;
 fn main() {
     let cli = CliArgs::parse();
 
-    println!("{:?}", cli);
+    match cli.commands {
+        cli_db::args::Command::Add(entry) => {
+            println!("Adding: {:?}", entry)
+        }
+        cli_db::args::Command::Remove(entry) => {
+            println!("Removing: {:?}", entry)
+        }
+        cli_db::args::Command::Get(key) => {
+            println!("Getting value for: {:?}", key)
+        }
+    }
 }
